d5376455113233cc18aa8c814a4428f2a2233485
vanegomez <vane.gomez@me.com>
2017-02-06 13:29:47 -0700

    index displays all skills

---
+++ app/assets/javascripts/components/_body.js.jsx
@@ -0,0 +1,10 @@
+var Body = React.createClass({
+  render() {
+    return (
+      <div>
+        <h1>Heading Body</h1>
+        <AllSkills />
+      </div>
+    )
+  }
+});
---
+++ app/assets/javascripts/components/_new_skill.js.jsx
@@ -0,0 +1,9 @@
+var Newskill = React.createClass({
+  render() {
+    return (
+      <div>
+        <h1>New Skill</h1>
+      </div>
+    )
+  }
+});
